5aa8dbcdf307c46527e648f3c1eeb6fe744cbb42
Farhan Ashraf bin Fakhrul Anwar <farhan3r3t@gmail.com>
2025-11-22 14:36:54 +0800

    First project

---
+++ studentrecordmanagementsystem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+struct student {
+    string name;
+    int age;
+    string matrixNum;
+    float GPA;
+
+};
+int main (){
+    cout << "----- Welcome to UTM Student File Handling System ----- " << endl;
+
+    return 0;
+}
