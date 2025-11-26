8ded0a576f4c48e49169db51df4a0eb1008c4358
Farhan Ashraf bin Fakhrul Anwar <farhan3r3t@gmail.com>
2025-11-26 14:26:51 +0800

    ++progress

--- matchaordersystem.cpp
+++ matchaordersystem.cpp
@@ -3,7 +3,7 @@
 #include <iomanip>
 using namespace std;
 struct order{
-    char drinkChoice;
+    int drinkChoice;
     int drinkAmount;
     char intensity;
     char milkChoice;
@@ -12,29 +12,54 @@ struct order{
     string Address;
     char time;
 };
+const int SIZE = 3;
+const vector<string> drinks {
+    "Matcha Latte", "Strawberry Matcha" , "Blueberry Matcha"
+};
+const vector<double> price {
+    8.00 , 9.50 , 9.5
+};
 void displayMenu ();
+template <typename T>
+void inputValidation(T &x , string y);
 
 
 int main (){
     vector<order> urOrder;
+    int choices;
     cout << " -------------------------------------------------------- " << endl;
     cout << " ----- Welcome to Farhan's Matcha Ordering System ! ----- " << endl;
     cout << " -------------------------------------------------------- " << endl;
     cout << " Our Menu : " << endl;
     displayMenu ();
+    inputValidation (choices , " How many different drink would you like to order ? : ");
+    urOrder.resize (choices);
     
 
     return 0;
 }
 void displayMenu (){
-    cout << " a. Matcha Latte - RM 8 " << endl << " a. Strawberry Matcha - RM 9.5 " << endl << " a. Blueberry Matcha - RM 9.5 ";
-}
-void chooseDrink(vector<order> urOrder){
-    for (int i =0; i < 3; i++ ) {
-        cout << "Choose your drink choice (a-c) : ";
-        cin >> urOrder[i].drinkChoice;
-        cout << "How many of this drink would you like ? : " ;
-        cin >>urOrder[i].drinkAmount;
+    for (int i=0; i < SIZE ; i++){
+        cout << i+1 << ". "<< drinks[i] << " - RM " << price[i] << endl;
     }
+}
+void chooseDrink(vector<order> &urOrder){
+    for (int i =0; i < urOrder.size(); i++ ) {
+        cout << "\nOrder #" << i+1 << " : " ;
+        inputValidation (urOrder[i].drinkChoice , "Choose your drink choice (1-3) : ");
+        inputValidation (urOrder[i].drinkAmount, "How many of this drink wouldd you like :");
 
 }
+}
+template <typename T>
+void inputValidation(T &x , string y){
+    cout << y;
+    while (true){
+        cin >> x;
+        if (cin.fail()){
+        cin.clear();
+        cin.ignore(1000 , '\n');
+        cout << "Invalid input ! Try again : ";
+        continue;  }
+     }
+     return;   }
