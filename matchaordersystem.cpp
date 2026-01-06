167f79d27b11432c690ca1e6123674053f4a62f8
Farhan Ashraf bin Fakhrul Anwar <farhan3r3t@gmail.com>
2026-01-06 17:21:20 +0800

    added intensity and syrup function

--- matchaordersystem.cpp
+++ matchaordersystem.cpp
@@ -4,14 +4,13 @@
 #include <string>
 using namespace std;
 struct order{
-    int drinkChoice;
-    int drinkAmount;
+
     char intensity;
     char milkChoice;
     bool syrup;
     char whisk;
     string Address;
-    char time;
+    double totalPice=0;
 };
 
 const int SIZE = 3;
@@ -24,8 +23,11 @@ const vector<double> price {
 void displayMenu ();
 template <typename T>
 void inputValidation(T &x , string y);
-void chooseDrink(vector<order> &urOrder);
-
+void chooseDrink(vector<order> &x);
+void intensity (vector<order> &x);
+void syrup (vector<order> &x);
+void milk (vector<order> &x);
+void whisk (vector<order> &x);
 
 int main (){
     vector<order> urOrder;
@@ -47,38 +49,38 @@ void displayMenu (){
 
 void chooseDrink(vector<order> &urOrder){
     int drinkTypes;
-
+    double price;
     inputValidation(drinkTypes, "How many different drinks would you like to order (max 3)? : ");
 
     for (int i = 0; i < drinkTypes; i++) {
 
         int choice, qty;
 
-        cout << "\nDrink #" << i + 1 << endl;
+        cout << endl <<"Drink Type #" << i + 1 << endl;
 
         do {
             inputValidation(choice, "Choose your drink (1-3): ");
         } while (choice < 1 || choice > 3);
+        int index = choice;
 
-        inputValidation(qty, "How many cups of this drink? ");
+        cout << endl << drinks[index-1] << endl << endl; 
+        inputValidation( qty , "How many cups of this drink? : ");
 
         for (int j = 0; j < qty; j++) {
             order temp;
-
-            temp.drinkChoice = choice;
-            temp.drinkAmount = 1;
-
+            temp.totalPice = price[index-1]*qty;
             cout << "Customisation for cup #" << j + 1 << endl;
 
-            inputValidation(temp.intensity, "Intensity (L/M/H): ");
-            inputValidation(temp.milkChoice, "Milk (O/D): ");
-            inputValidation(temp.syrup, "Add syrup? (1=Yes, 0=No): ");
+            inputValidation(temp.milkChoice, "Milk (O (Oatmilk) / D (Dairy) ): ");
+            inputValidation(temp.whisk, "Whisk Method? (A = Hot Whisk , B = Cold Whisk): ");
+            inputValidation(temp.Address, "Enter your address : ");
+
+
 
             urOrder.push_back(temp);
         }
+    }
 }
-}
-
 template <typename T>
 void inputValidation(T &x , string y){
     cout << y;
@@ -92,3 +94,22 @@ void inputValidation(T &x , string y){
         }
      break;}
  }
+void intensity (vector<order> &x){
+    inputValidation (x.intensity , "Intensity (N = Normal / H = High) : ");
+    x.intesity = toupper (x.intensity);
+    if ( x.intensity == H){
+        x.totalPrice += 1.00;
+    }
+}
+void syrup (vector<order> &x){
+
+    inputValidation(x.syrup, "Add syrup? (1 = Yes, 0 = No): ");
+    if (x.syrup == 1){
+        x.totalPrice += 0.50;
+    }
+
+
+}
+
+
+
